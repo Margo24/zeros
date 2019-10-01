9041d12998f7171f99c55c88dde3516079a6e233
‘Margo24’ <margonessa24@gmail.com>
2019-10-01 08:54:09 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = function zeros(expression) {
 
     let fives = 0;
@@ -30,36 +29,3 @@ module.exports = function zeros(expression) {
     return (fives<twos) ? fives:twos;
 
 }
-=======
-module.exports = function zeros(expression) {
-
-    let fives = 0;
-    let twos = 0;
-    let decr = 1;
-    let stringArr = expression.split("*");
-    let divNum=0;
-    for (i = 0; i<stringArr.length; i++) {
-        const a = parseInt(stringArr[i]);
-        const b = a.toString();
-        decr = ((stringArr[i].length - b.length)===1) ? 1:2;
-        console.log(a);
-        //console.log(decr);
-        for (let y=a; y>0; ) {
-            let divNum=0;
-            if (y%2===0) {
-                twos++;
-            }
-            if (y%5===0) {
-                divNum=y;
-                while (divNum%5===0){
-                    divNum=divNum/5;
-                    fives++;
-                }
-            }
-            y-=decr;
-        }
-    }
-    return (fives<twos) ? fives:twos;
-
-}
->>>>>>> c0a0fcb56fb42133fb40e60bc31b8664f28a8242
